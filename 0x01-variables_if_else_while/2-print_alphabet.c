8c237a03d5b9068f027f6608b61de48fdb4f6edf
root <3396@holbertonschool.com>
2021-06-19 08:37:19 -0700

    script 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int Alpha = 97;
+
+while (Alpha <= 122)
+{
+putchar(Alpha);
+Alpha++;
+}
+putchar ('\n');
+return (0);
+}
