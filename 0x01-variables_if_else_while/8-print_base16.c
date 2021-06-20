c38b8c555b3a81b7ce6e0a75d3a92eba87768fc1
root <3396@holbertonschool.com>
2021-06-19 17:15:41 -0700

    programa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int number;
+int alpha;
+for(number = 48; number <=57; number++)
+{
+putchar(number);
+}
+for(alpha =97; alpha <= 102; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
