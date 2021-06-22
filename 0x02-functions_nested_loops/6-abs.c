5fc3b2ac9e106088e84571b4cc29fa572d3e1dce
kenialo19 <you@example.com>
2021-06-22 14:23:56 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 /**
  * int _abs - computes the absolute value of an integer.
- * @int: absolute value
+ * @x: absolute value
  *
- * x: whole
- * abs: Returns the absolute value of a number
- * Return: On success 1.
+ * Return: value absolute
  */
 int _abs(int x)
 {
