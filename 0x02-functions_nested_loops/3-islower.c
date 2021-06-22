d94bf64c0113c31aed1c5dd9fc0a72e79f01cfcb
kenialo19 <you@example.com>
2021-06-22 07:43:30 -0700

    programa

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (islower(c))
+{
+return (1);
+}
+return (0);
+}
