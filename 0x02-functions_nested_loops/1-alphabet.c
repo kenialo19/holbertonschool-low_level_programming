f0184408a3e07e183cca5453747ca8e32b086b3f
kenialo19 <you@example.com>
2021-06-21 20:43:30 -0700

    programa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int Alpha;
+
+for (Alpha = 97; Alpha <= 122; Alpha++)
+{
+_putchar(Alpha);
+}
+_putchar ('\n');
+}
