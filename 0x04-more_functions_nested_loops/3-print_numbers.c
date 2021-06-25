1c31a6e3ccd7d697a916b771e8d1c906d988d783
kenialo19 <you@example.com>
2021-06-25 13:43:43 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * @int: number
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int num = 48;
+
+while (num <= 57)
+{
+_putchar(num);
+num++;
+}
+_putchar('\n');
+}
