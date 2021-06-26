7336c12725eab910074706f4a3bc9130799bb828
kenialo19 <you@example.com>
2021-06-25 21:04:08 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal.
+ * @n: number
  *
  * Return: Always 0.
  */
@@ -11,6 +12,6 @@ int x;
 for (x = 0; x < n; x++)
 {
 _putchar('_');
-}   
+}
 _putchar ('\n');
 }
