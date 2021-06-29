d4d54704189a167739d48cbf8c14669df82dc789
kenialo19 <you@example.com>
2021-06-28 22:06:52 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,9 @@
 void puts_half(char *str)
 {
 int j, h, mitad;
-j = (strlen(str)-1);
+j = (strlen(str));
 mitad = j / 2;
-for (h = mitad; h <= j; h++)
+for (h = mitad; h < j; h++)
 {
 _putchar(str[h]);
 }
