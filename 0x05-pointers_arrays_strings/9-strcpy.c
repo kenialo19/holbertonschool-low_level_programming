ab65de6ec5e051756eb04d85046500ff39878b74
kenialo19 <you@example.com>
2021-06-29 22:00:40 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest: string
+ * @src: string
+ *
+ * Return: the pointer to dest
+ */
+ char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
