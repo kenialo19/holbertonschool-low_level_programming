17f1d20d175cce886b8aefeeda10d480dcc4cccb
kenialo19 <you@example.com>
2021-07-03 13:09:30 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ *  *_strncpy - copies a string.
+ * @dest: string.
+ * @src: string.
+ * @n: whole
+ *
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int a = strlen(dest);
+int b = strlen(src);
+int k, s;
+if (b < n)
+{
+n = b;
+}
+for (k = 0; k < n; k++)
+{
+dest[k] = src[k];
+}
+for (s = b; s <= n; s++)
+{
+dest[s] = 0;
+}
+dest[a] = 0;
+return (dest);
+}
