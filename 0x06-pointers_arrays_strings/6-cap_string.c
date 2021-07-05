649bfb8feda173fd6d9b1a5c8912db9dfee35641
kenialo19 <you@example.com>
2021-07-05 14:16:41 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 #include <string.h>
 
-
 /**
  * *cap_string - capitalizes all words of a string.
  * @b: string
@@ -15,7 +14,7 @@ if (b[0] >= 'a' && b[0] <= 'z')
 {
 b[0] = b[0] - 32;
 }
-for(d = 0; b[d] != 0; d++)
+for (d = 0; b[d] != 0; d++)
 {
 switch (b[d])
 {
@@ -25,12 +24,11 @@ case '.':
 case ' ':
 case '\n':
 case '\t':
-if(b[d + 1] > 96 && b[d + 1] < 123)
+if (b[d + 1] > 96 && b[d + 1] < 123)
 {
 b[d + 1] = b[d + 1] - 32;
-}         
 }
-}  
-return (b); 
 }
- 
+}
+return (b);
+}
