f3abda81793136a529dbcb45e061c24784c89e85
kenialo19 <you@example.com>
2021-07-05 14:41:51 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * *leet - encodes a string into 1337.
+ * @k: string
+ *
+ * Return: k.
+ */
+char *leet(char *k)
+{
+int h, d;
+char p[] = "AaEeOoTtLl";
+char w[] = "4433007711";
+for (h = 0; k[h]; h++)
+{
+for (d = 0; d < 10; d++)
+{
+if (k[h] == p[d])
+{
+k[h] = w[d];
+}
+}
+return (k);
+}
