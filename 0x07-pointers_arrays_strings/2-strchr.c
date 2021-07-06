8c5e9179f36276822a748f0a0f70e9797dd8510d
kenialo19 <you@example.com>
2021-07-06 09:54:31 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,11 @@
 char *_strchr(char *s, char c)
 {
 int x;
-for (x = 0; s[x] > x; x++)
+for (x = 0; s[x] != '\0'; x++)
 {
 if (s[x] == c)
 {
-return (&s[x]);
+return (s + x);
 }
 }
 return ('\0');
