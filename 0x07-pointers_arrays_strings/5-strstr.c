8cc7accfd8154047be6d67725bd5db67e27e8f71
kenialo19 <you@example.com>
2021-07-06 23:22:46 -0700

    strt

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,13 +10,22 @@
 char *_strstr(char *haystack, char *needle)
 {
 int b;
-for (b = 0; haystack[b] != '\0'; b++)
+if (needle == 0)
 {
-if (haystack[b] == needle[0])
+return (haystack);
+}
+for (b = 0; haystack[b]; b++)
 {
-return (haystack + b);
+if (haystack[b] == needle[b])
+{
+do {
+if (needle[b + 1] == '\0')
+{
+return (haystack);
+}
+b++;
+} while (haystack[b] == needle[b]);
 }
 }
-return (0);
+return ('\0');
 }
-
