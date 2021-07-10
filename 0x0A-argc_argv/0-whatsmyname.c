61cfe4c7b2b400c9ec911ba108bbeff8c0e20c5b
kenialo19 <you@example.com>
2021-07-09 20:21:58 -0700

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,13 @@
 
 /**
  * main - check the code for Holberton School students.
- * @argc: count the lines argument.
- * @argv: argument vector
+ * @argcUnused: unused variable.
+ * @argv: argument vector.
  *
  * Return: Always 0.
  */
-
-int main (int argcUnused __attribute__((unused)), char *argv[])
+int main(int argcUnused __attribute__((unused)), char *argv[])
 {
-  printf("%s\n", argv[0]);
-  return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
