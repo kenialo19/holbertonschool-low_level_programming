86fbe6a19fb7424d99239e0ff0757515ac791fe2
kenialo19 <you@example.com>
2021-07-09 23:07:23 -0700

    multi

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: unused variable.
+ * @argv: unused variable.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int x1;
+int x2;
+int x;
+if (argc == 3)
+{
+x2 = atoi(argv[2]);
+x1 = atoi(argv[1]);
+x = x1 *x2;
+printf("%d\n", x);
+}
+else
+{
+printf("error\n");
+return (1);
+}
+return (0);
+}
