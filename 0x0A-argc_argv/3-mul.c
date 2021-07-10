ee4c6d6cdab930fc2059d96972aa05041473f1e2
kenialo19 <you@example.com>
2021-07-09 23:47:20 -0700

    mult

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,17 @@
 int main(int argc, char *argv[])
 {
 int x1, x2, x;
+
 if (argc == 3)
 {
 x1 = atoi(argv[1]);
 x2 = atoi(argv[2]);
-x = x1 * x2;
+x = x1 *x2;
 printf("%d\n", x);
 }
 else
 {
-printf("error\n");
+printf("Error\n");
 return (-1);
 }
 return (0);
