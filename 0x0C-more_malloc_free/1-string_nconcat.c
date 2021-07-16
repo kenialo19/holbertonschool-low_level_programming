b3e691715716b34608cf0bfc8dedac10e2e5d1c0
kenialo19 <you@example.com>
2021-07-16 16:39:33 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,51 +7,46 @@
  *
  * Return: Always 0.
  */
- char *string_nconcat(char *s1, char *s2, unsigned int n)
- {
-     unsigned int i =  0;
-     unsigned int j = 0;
-     unsigned  int k;
-     char *b;
-     if (s1 != NULL)
-       {
-     while (s1[i] != '\0')
-     {
-         i++;
-     }
-       }
-     if (s2 != NULL)
-       {
-     while (s2[j] != '\0')
-     {
-         j++;
-     }
-       }
-     if (n > j)
-    {
-         n = j; 
-    }
-     b = malloc((i + n + 1) * sizeof(char));
-     
-     if (b == 0)
-    {
-        return (0);
-    }
-     
-      for (k = 0; k < i + n; k++)
-    {
-        if (k < i)
-    {
-         b[k] = s1[k];
-    }
-   else
-    {
-       b[k] = s2[k - i];
-
-    }
-    }
-    b[k] = '\0';
-    return (b);
-
- }
- 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i =  0;
+unsigned int j = 0;
+unsigned  int k;
+char *b;
+if (s1 != NULL)
+{
+while (s1[i] != '\0')
+{
+i++;
+}
+}
+if (s2 != NULL)
+{
+while (s2[j] != '\0')
+{
+j++;
+}
+}
+if (n > j)
+{
+n = j;
+}
+b = malloc((i + n + 1) * sizeof(char));
+if (b == 0)
+{
+return (0);
+}
+for (k = 0; k < i + n; k++)
+{
+if (k < i)
+{
+b[k] = s1[k];
+}
+else
+{
+b[k] = s2[k - i];
+}
+}
+b[k] = '\0';
+return (b);
+}
