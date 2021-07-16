293bd6511be91b86c26ec50de9fee9a43974bd66
kenialo19 <you@example.com>
2021-07-16 13:46:48 -0700

    concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ char *string_nconcat(char *s1, char *s2, unsigned int n)
+ {
+     unsigned int i =  0;
+     unsigned int j = 0;
+     unsigned  int k;
+     char *b;
+     
+     while (s1[i] != '\0')
+     {
+         i++;
+     }
+     
+     while (s2[j] != '\0')
+     {
+         j++;
+     }
+    
+     b = malloc((i + j) * sizeof(char));
+     
+     if (b == 0)
+    {
+        return (0);
+    }
+     for (k = 0; k < i + n; k++)
+    {
+        if (k < i)
+    {
+         b[k] = s1[k];
+    }
+    else
+    {
+        b[k] = s2[k - i];
+    }
+    }
+    return (b);
+    
+ }
