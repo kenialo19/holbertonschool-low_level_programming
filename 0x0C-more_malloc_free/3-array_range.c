433739190f4ce872f1f6ddfa21e243bce7cf41e1
kenialo19 <you@example.com>
2021-07-16 21:25:51 -0700

    j

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,27 +9,26 @@
  * Return: a pointer.
  */
 int *array_range(int min, int max)
-{ int *h = NULL;
-  int n, k;
-
-  if (min > max)
-  {
-    return (NULL);
-  }
-  k = max - min;
-
-  h = malloc((k + 1) * sizeof(int));
-  if (h == NULL)
 {
-  return (NULL);
+int *h = NULL;
+int n, k;
+if (min > max)
+{
+return (NULL);
 }
-  for (n = 0; n <= k; n++)
-    {
-      if (min <= max)
-      {
-      h[n] = min;
-      min++;
-    }
-      }
-  return (h);
+k = max - min;
+h = malloc((k + 1) * sizeof(int));
+if (h == NULL)
+{
+return (NULL);
+}
+for (n = 0; n <= k; n++)
+{
+if (min <= max)
+{
+h[n] = min;
+min++;
+}
+}
+return (h);
 }
