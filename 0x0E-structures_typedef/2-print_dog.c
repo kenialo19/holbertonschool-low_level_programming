da0d810719ebcec1ecf8478416f91e5b8b870d9c
kenialo19 <you@example.com>
2021-07-20 17:22:44 -0700

    printf dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,16 +13,23 @@ if (d == NULL)
 {}
 else 
 {
-  if (d->name == NULL)
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
+printf("Age: %f\n", d->age);
+ if (d->owner == NULL)
    {
      printf("Name: (nil)\n");
    }
-	else
-	{
-printf("Name: %s\n", d->name);
-	}
-printf("Age: %f\n", d->age);
+ else
+   {
 printf("Owner: %s\n", d->owner);
 }
+ }
 }
  
