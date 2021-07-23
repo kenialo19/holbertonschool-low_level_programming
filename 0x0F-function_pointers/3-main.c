d3dcc3dfb4d6b53259f6aa0728a1e5acfe11c03a
kenialo19 <you@example.com>
2021-07-22 18:25:20 -0700

    .

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: count the lines argument.
+ * @argv: arguments vector.
+ *
+ * Return: Always 0.
+ */
+
+int main (int argc, char *argv[])
+{
+int a, b, result;
+char *operator = NULL;
+int (*f)(int a, int b);
+if (argc > 4 || argc < 4)
+{
+printf("Error\n");
+exit(98);
+}
+operator = argv[2];
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+f = get_op_func(operator);
+if (!f)
+{
+printf("Error\n");
+exit(99);
+}
+result = f(a,b);
+printf("%d\n", result);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum
+ * @a:
+ * @b: 
+ */
+
+
+int op_add(int a, int b)
+{
+  return (a + b);
+}
+
+/**
+ * op_add - sum
+ * @a:
+ * @b:
+ */
+
+int op_sub(int a, int b)
+{
+  return (a - b);
+}
+
+/**
+ * op_add - sum
+ * @a:
+ * @b:
+ */
+
+int op_mul(int a, int b)
+{
+  return (a * b);
+}
+
+/**
+ * op_add - sum
+ * @a:
+ * @b:
+ */
+
+int op_div(int a, int b)
+{
+  return (a / b);
+}
+/**
+ * op_add - sum
+ * @a:
+ * @b:
+ */
+
+int op_mod(int a, int b)
+{
+  return (a % b);
+}
