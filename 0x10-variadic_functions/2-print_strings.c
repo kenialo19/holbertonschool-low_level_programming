deb35758099ca5eba1a281d7edcfad057dddff7e
kenialo19 <you@example.com>
2021-07-23 22:39:35 -0700

    print dtring

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,8 +16,6 @@ unsigned int i;
 char *ptr;
 va_list args;
 va_start(args, n);
-if (separator != NULL)
-{
 for (i = 0; i < n; i++)
 {
 ptr = va_arg(args, char *);
@@ -25,15 +23,16 @@ if (ptr == NULL)
 {
 printf("(nil)");
 }
+else if (separator != NULL)
+{
 printf("%s", ptr);
 if (i < (n - 1))
 printf("%s", separator);
 }
-}
 else
 {
-ptr = va_arg(args, char *);
 printf("%s", ptr);
+}
 }
 va_end(args);
 printf("\n");
