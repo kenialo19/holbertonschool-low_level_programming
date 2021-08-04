385fd71b49fd86169b08f9730ee56b57c63c1964
kenialo19 <3396@holbertonschool.com>
2021-08-03 23:54:52 -0500

    p

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - function that prints a listint_t linked list.
+ * @head: pointer list.
+ *
+ * Return: elements list.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int i = 0;
+
+	if (head == NULL)
+	{
+		return (98);
+	}
+
+	while (head->next != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+		i++;
+	}
+	return (i);
+}
