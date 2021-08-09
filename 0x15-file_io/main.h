ff96efb0522a393417ac91cd871a155c85de917b
kenialo19 <3396@holbertonschool.com>
2021-08-09 11:50:14 -0500

    prototype files have been added

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,15 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <limits.h>
+
 /* PROTYPES */
 
 int _putchar(char c);
