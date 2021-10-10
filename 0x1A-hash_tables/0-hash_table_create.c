13f936a1d3246d46bd04a5f491da9b36067b6c5d
kenialo19 <3396@holbertonschool.com>
2021-10-10 17:29:20 -0500

    modified code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 /**
  * *hash_table_create -  function that creates a hash table.
  * @size: is the size of the array
- * Return: new table. 
+ * Return: new table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -11,18 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-	{
-		free(new_table);
 		return (NULL);
-	}
 
 	new_table->size = size;
 	new_table->array = calloc(size, sizeof(hash_table_t *));
-	if (new_table == NULL)
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	free(new_table);
 	return (new_table);
 }
