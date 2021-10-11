ac51bd5befd7a748fd26ed6e544bdbce3e4d8960
kenialo19 <3396@holbertonschool.com>
2021-10-10 19:59:34 -0500

    modified file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,25 +10,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node = NULL;
-	hash_node_t *aux = NULL;
 	unsigned long int index = 0;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	aux = ht->array[index];
 
-	while (aux != NULL)
-	{
-		if (strcmp(aux->key, key) == 0)
-		{
-			free(aux->value);
-			aux->value = strdup(value);
-			return (1);
-		}
-		aux = aux->next;
-	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
